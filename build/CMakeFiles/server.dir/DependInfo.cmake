4e60c869e6cffaae6bcd144e47776378cdc88715
Ayden <aydenwhittaker2014@gmail.com>
2025-05-03 14:48:51 -0500

    After running the build and server file

---
+++ build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/kellu/OneDrive/Documents/GitHub/OS-Project-3/src/io_helper.c" "CMakeFiles/server.dir/src/io_helper.c.o" "gcc" "CMakeFiles/server.dir/src/io_helper.c.o.d"
+  "/mnt/c/Users/kellu/OneDrive/Documents/GitHub/OS-Project-3/src/request.c" "CMakeFiles/server.dir/src/request.c.o" "gcc" "CMakeFiles/server.dir/src/request.c.o.d"
+  "/mnt/c/Users/kellu/OneDrive/Documents/GitHub/OS-Project-3/src/wserver.c" "CMakeFiles/server.dir/src/wserver.c.o" "gcc" "CMakeFiles/server.dir/src/wserver.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
